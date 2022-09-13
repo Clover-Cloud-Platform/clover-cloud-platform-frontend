364c9b96b7cc6e932557f96527cc0a42147a948b
DevMBS <mbs.fpv@gmail.com>
2022-09-13 19:43:43 +0300

    + AppContainer with app controls

--- src/Components/Terminal.js
+++ src/Components/Terminal.js
@@ -13,19 +13,9 @@ let historyIndex = 0;
 let down = false;
 let up = false;
 let history = [];
-let fullScreenMode = false;
-let prevTermPosition = false;
-let prevTermSize = false;
 
-export default function Terminal() {
+export default function Terminal(props) {
   const [output, setOutput] = React.useState([]);
-  const [draggable, disableDraggable] = React.useState(false);
-  const [minimized, setMinimized] = React.useState(true);
-  const [size, setSize] = React.useState({width: 700, height: 400});
-  const [position, setPosition] = React.useState({
-    x: window.innerWidth / 2 - 350,
-    y: window.innerHeight / 2 - 200,
-  });
 
   const execute = command => {
     if (command.trim() !== "") {
@@ -54,13 +44,18 @@ export default function Terminal() {
   };
 
   const handleKeyDown = e => {
-    let input = document.getElementById("terminalInput");
+    const moveCaretToEnd = () => {
+      e.target.setSelectionRange(
+        e.currentTarget.value.length,
+        e.currentTarget.value.length,
+      );
+    };
     if (e.key === "Enter") {
-      execute(input.value);
-      history.unshift(input.value);
+      execute(e.currentTarget.value);
+      history.unshift(e.currentTarget.value);
       historyKey++;
       historyIndex = 0;
-      input.value = "";
+      e.currentTarget.value = "";
     } else if (e.key === "ArrowUp") {
       if (historyIndex < historyKey) {
         if (down) {
@@ -68,11 +63,9 @@ export default function Terminal() {
           down = false;
         }
         if (history[historyIndex]) {
-          input.value = history[historyIndex];
+          e.currentTarget.value = history[historyIndex];
         }
-        setTimeout(() => {
-          input.selectionStart = input.selectionEnd = input.value.length;
-        }, 1);
+        moveCaretToEnd();
         historyIndex++;
         up = true;
       }
@@ -86,16 +79,14 @@ export default function Terminal() {
         }
         down = true;
         if (history[historyIndex]) {
-          input.value = history[historyIndex];
+          e.currentTarget.value = history[historyIndex];
         }
-        setTimeout(() => {
-          input.selectionStart = input.selectionEnd = input.value.length;
-        }, 1);
+        moveCaretToEnd();
       } else {
         down = false;
         up = false;
         historyIndex = 0;
-        input.value = "";
+        e.currentTarget.value = "";
       }
     }
   };
@@ -123,160 +114,41 @@ export default function Terminal() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Rnd
-        enableResizing={!draggable}
-        disableDragging={draggable}
-        minHeight={200}
-        minWidth={250}
-        size={size}
-        position={position}
-        onDragStop={(e, d) => {
-          setPosition({x: d.x, y: d.y});
-          prevTermPosition = {x: d.x, y: d.y};
-        }}
-        onResizeStop={(e, direction, ref, delta, position) => {
-          setSize({
-            width: ref.style.width,
-            height: ref.style.height,
-          });
-          prevTermSize = {width: ref.style.width, height: ref.style.height};
-          prevTermPosition = position;
-          setPosition(position);
-        }}
-        resizeHandleStyles={{
-          left: {cursor: "e-resize"},
-          right: {cursor: "ew-resize"},
-          top: {cursor: "s-resize"},
-          bottom: {cursor: "s-resize"},
+      <Box
+        sx={{
+          position: "absolute",
+          overflowY: "scroll",
+          left: 10,
+          right: 10,
+          top: 25,
+          bottom: 10,
         }}>
-        <Fade in={minimized}>
-          <Box
-            sx={{cursor: "auto"}}
-            bgcolor={theme.palette.background.default}
-            boxShadow={"4px 4px 26px 1px rgba(0, 0, 0, 0.47)"}
-            borderRadius={"18px"}
-            position={"absolute"}
-            m={"auto"}
-            left={0}
-            right={0}
-            top={0}
-            bottom={0}>
-            <Box mb={"10px"} ml={"14px"} mr={"14px"}>
-              <Box position={"absolute"} top={0} right={0} display={"flex"}>
-                <Box
-                  m={"1px"}
-                  p={"5px"}
-                  onClick={() => {
-                    setMinimized(prev => !prev);
-                  }}>
-                  <Tooltip title={"Minimize"}>
-                    <CircleIcon
-                      sx={{
-                        fontSize: 17,
-                        color: theme.palette.success.light,
-                        ":hover": {color: theme.palette.success.dark},
-                      }}
-                    />
-                  </Tooltip>
-                </Box>
-                <Box
-                  m={"1px"}
-                  p={"5px"}
-                  onClick={() => {
-                    if (!fullScreenMode) {
-                      fullScreenMode = true;
-                      setPosition({
-                        x: 0,
-                        y: 0,
-                      });
-                      setSize({width: "100%", height: "100%"});
-                      disableDraggable(true);
-                    } else {
-                      setSize(prevTermSize);
-                      setPosition(prevTermPosition);
-                      fullScreenMode = false;
-                      disableDraggable(false);
-                    }
-                  }}>
-                  <Tooltip
-                    title={fullScreenMode ? "Window Mode" : "Fullscreen Mode"}>
-                    <CircleIcon
-                      sx={{
-                        fontSize: 17,
-                        color: theme.palette.warning.light,
-                        ":hover": {color: theme.palette.warning.dark},
-                      }}
-                    />
-                  </Tooltip>
-                </Box>
-                <Box
-                  m={"1px"}
-                  p={"5px"}
-                  onClick={() => {
-                    setMinimized(prev => !prev);
-                  }}>
-                  <Tooltip title={"Close"}>
-                    <CircleIcon
-                      sx={{
-                        fontSize: 17,
-                        color: theme.palette.error.light,
-                        ":hover": {color: theme.palette.error.dark},
-                      }}
-                    />
-                  </Tooltip>
-                </Box>
-              </Box>
-              <Box
-                sx={{
-                  position: "absolute",
-                  overflowY: "scroll",
-                  left: 10,
-                  right: 10,
-                  top: 25,
-                  bottom: 10,
-                }}>
-                <Box
-                  display={"flex"}
-                  flexDirection={"column-reverse"}
-                  onMouseOut={() => {
-                    if (!fullScreenMode) {
-                      disableDraggable(false);
-                    }
-                  }}
-                  onMouseOver={() => {
-                    disableDraggable(true);
-                  }}>
-                  <Box display={"flex"}>
-                    <Typography
-                      color={theme.palette.text.dir}
-                      fontFamily={"Monospace"}>
-                      ~$
-                    </Typography>
-                    <input
-                      id={"terminalInput"}
-                      autoFocus
-                      onKeyDown={handleKeyDown}
-                      style={{
-                        background: "transparent",
-                        width: "100%",
-                        outline: "none",
-                        border: "none",
-                        color: theme.palette.text.primary,
-                        fontFamily: "Monospace",
-                        fontWeight: 400,
-                        fontSize: "1rem",
-                        lineHeight: 1.5,
-                        letterSpacing: "0.00938em",
-                      }}
-                    />
-                  </Box>
-                  {output}
-                </Box>
-              </Box>
-            </Box>
+        <Box display={"flex"} flexDirection={"column-reverse"}>
+          <Box display={"flex"}>
+            <Typography color={theme.palette.text.dir} fontFamily={"Monospace"}>
+              ~$
+            </Typography>
+            <input
+              id={"terminalInput"}
+              autoFocus
+              onKeyDown={handleKeyDown}
+              style={{
+                background: "transparent",
+                width: "100%",
+                outline: "none",
+                border: "none",
+                color: theme.palette.text.primary,
+                fontFamily: "Monospace",
+                fontWeight: 400,
+                fontSize: "1rem",
+                lineHeight: 1.5,
+                letterSpacing: "0.00938em",
+              }}
+            />
           </Box>
-        </Fade>
-      </Rnd>
+          {output}
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 }
