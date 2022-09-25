7c49368bb00d98ed16351739beb3e0ccf55751c5
DevMBS <mbs.fpv@gmail.com>
2022-09-25 22:44:13 +0300

    add email verification

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -33,6 +33,8 @@ export const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
 
+let timerCount = 60;
+
 export default function SignUp() {
   const [usernameError, setUsernameError] = React.useState(false);
   const [emailError, setEmailError] = React.useState(false);
@@ -40,6 +42,12 @@ export default function SignUp() {
   const [usernameHelper, setUsernameHelper] = React.useState("");
   const [emailHelper, setEmailHelper] = React.useState("");
   const [passwordHelper, setPasswordHelper] = React.useState("");
+  const [verify, setVerify] = React.useState(false);
+  const [codeError, setCodeError] = React.useState(false);
+  const [codeHelper, setCodeHelper] = React.useState("");
+  const [authCode, setAuthCode] = React.useState();
+  const [timer, setTimer] = React.useState(60);
+  const [resend, disableResend] = React.useState(true);
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -56,6 +64,17 @@ export default function SignUp() {
         "Password must contain minimum 6 characters, at least one letter and one number",
       );
     } else {
+      setVerify(true);
+      const timerInterval = setInterval(() => {
+        if (timerCount > 0) {
+          setTimer(prev => prev - 1);
+          timerCount--;
+        } else {
+          clearInterval(timerInterval);
+          setTimer();
+          disableResend(false);
+        }
+      }, 1000);
       socket.emit("SignUp", {
         username: username,
         email: email,
@@ -68,105 +87,186 @@ export default function SignUp() {
         } else if (res.error === "email") {
           setEmailError(true);
           setEmailHelper("There is already an account with this email");
-        } else if (!res.error && res.uid) {
-          localStorage.setItem("uid", res.uid);
-          window.location.href = "/clover";
+        } else if (!res.error && res.code) {
+          setAuthCode(res.code);
         }
       });
     }
   };
 
+  const handleAuthCodeSubmit = event => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const enteredCode = data.get("authCode").trim();
+    if (enteredCode === authCode) {
+      socket.emit("AuthByEmail", true);
+    }
+  };
+
+  const resendCode = e => {
+    e.preventDefault();
+    socket.emit("AuthByEmail", false);
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <Fade in={true}>
-        <Box>
-          <Container component="main" maxWidth="xs">
-            <Box
-              sx={{
-                marginTop: 8,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}>
-              <Avatar sx={{m: 1, bgcolor: "primary.400"}}>
-                <LockOutlinedIcon />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Sign up
-              </Typography>
-              <Box component="form" onSubmit={handleSubmit} sx={{mt: 3}}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <TextField
-                      onChange={() => {
-                        setUsernameError(false);
-                        setUsernameHelper("");
-                      }}
-                      error={usernameError}
-                      helperText={usernameHelper}
-                      autoComplete="username"
-                      name="username"
-                      required
-                      fullWidth
-                      id="username"
-                      label="Username"
-                      autoFocus
-                    />
+      {!verify ? (
+        <Fade in={true}>
+          <Box>
+            <Container component="main" maxWidth="xs">
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}>
+                <Avatar sx={{m: 1, bgcolor: "primary.400"}}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign up
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} sx={{mt: 3}}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        onChange={() => {
+                          setUsernameError(false);
+                          setUsernameHelper("");
+                        }}
+                        error={usernameError}
+                        helperText={usernameHelper}
+                        autoComplete="username"
+                        name="username"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        autoFocus
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        onChange={() => {
+                          setEmailError(false);
+                          setEmailHelper("");
+                        }}
+                        error={emailError}
+                        helperText={emailHelper}
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        onChange={() => {
+                          setPasswordError(false);
+                          setPasswordHelper("");
+                        }}
+                        error={passwordError}
+                        helperText={passwordHelper}
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                      />
+                    </Grid>
                   </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      onChange={() => {
-                        setEmailError(false);
-                        setEmailHelper("");
-                      }}
-                      error={emailError}
-                      helperText={emailHelper}
-                      required
-                      fullWidth
-                      id="email"
-                      label="Email Address"
-                      name="email"
-                      autoComplete="email"
-                    />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{mt: 3, mb: 2}}>
+                    Sign Up
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                    <Grid item>
+                      <Link href="/signin" variant="body2">
+                        Already have an account? Sign in
+                      </Link>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      onChange={() => {
-                        setPasswordError(false);
-                        setPasswordHelper("");
-                      }}
-                      error={passwordError}
-                      helperText={passwordHelper}
-                      required
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      id="password"
-                      autoComplete="new-password"
-                    />
-                  </Grid>
-                </Grid>
+                </Box>
+              </Box>
+              <Copyright sx={{mt: 5}} />
+            </Container>
+          </Box>
+        </Fade>
+      ) : (
+        <Fade in={true}>
+          <Box>
+            <Container component="main" maxWidth="lg">
+              <Box
+                component="form"
+                onSubmit={handleAuthCodeSubmit}
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  height: "100vh",
+                }}>
+                <Typography
+                  sx={{
+                    fontFamily: "Google Sans,Noto Sans,sans-serif",
+                    letterSpacing: "-.5px",
+                    lineHeight: "1.2em",
+                    fontSize: "24px",
+                    "@media (min-width:900px)": {fontSize: "30px"},
+                    textAlign: "center",
+                    mb: "30px",
+                  }}>
+                  Enter your auth code we sent you by email
+                </Typography>
+                <TextField
+                  onChange={() => {
+                    setCodeError(false);
+                    setCodeHelper("");
+                  }}
+                  error={codeError}
+                  helperText={codeHelper}
+                  autoComplete="authCode"
+                  name="authCode"
+                  required
+                  id="authCode"
+                  label="Auth code"
+                  autoFocus
+                  sx={{maxWidth: "400px", width: "100%"}}
+                />
                 <Button
                   type="submit"
-                  fullWidth
                   variant="contained"
-                  sx={{mt: 3, mb: 2}}>
-                  Sign Up
+                  sx={{mt: 3, maxWidth: "400px", width: "100%"}}>
+                  Verify
                 </Button>
-                <Grid container justifyContent="flex-end">
+                <Grid
+                  container
+                  justifyContent="flex-end"
+                  maxWidth={"400px"}
+                  width={"100%"}
+                  mt={3}>
                   <Grid item>
-                    <Link href="/signin" variant="body2">
-                      Already have an account? Sign in
-                    </Link>
+                    <Button
+                      size={"small"}
+                      onClick={resendCode}
+                      disabled={resend}>
+                      Resend code {timer}
+                    </Button>
                   </Grid>
                 </Grid>
               </Box>
-            </Box>
-            <Copyright sx={{mt: 5}} />
-          </Container>
-        </Box>
-      </Fade>
+            </Container>
+          </Box>
+        </Fade>
+      )}
     </ThemeProvider>
   );
 }
