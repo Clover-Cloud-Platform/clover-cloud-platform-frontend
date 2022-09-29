09a475106a3d1797d728521e5511ba0919bb3389
DevMBS <mbs.fpv@gmail.com>
2022-09-29 22:19:59 +0300

    move to firebase

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import MainApp from "./Components/MainApp";
 import SignUp from "./Components/SignUp";
 import SignIn from "./Components/SignIn";
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: "clover-cloud-platform",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
