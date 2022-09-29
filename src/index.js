b8f4d30f5a0753cf6c700530a3b99d09eb71c357
DevMBS <mbs.fpv@gmail.com>
2022-09-29 22:29:50 +0300

    move to firebase

--- src/index.js
+++ src/index.js
@@ -9,17 +9,15 @@ import SignUp from "./Components/SignUp";
 import SignIn from "./Components/SignIn";
 import {initializeApp} from "firebase/app";
 import {getAnalytics} from "firebase/analytics";
-import * as dotenv from "dotenv";
-dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: "clover-cloud-platform",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
