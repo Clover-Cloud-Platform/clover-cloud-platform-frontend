d61aaa04027bd2bcecd3415dba874a84c1ccf0b0
DevMBS <mbs.fpv@gmail.com>
2022-10-01 13:37:23 +0300

    analytics

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import MainApp from "./Components/MainApp";
 import SignUp from "./Components/SignUp";
 import SignIn from "./Components/SignIn";
+import {initializeApp} from "firebase/app";
+import {getAnalytics, logEvent} from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: "clover-cloud-platform",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
