93192227b6790587b48c706d76b9b5f210e53c30
DevMBS <mbs.fpv@gmail.com>
2023-01-14 16:44:52 +0300

    file manager

--- src/Components/FileManager.js
+++ src/Components/FileManager.js
@@ -10,45 +10,148 @@ import SettingsBackupRestoreRoundedIcon from "@mui/icons-material/SettingsBackup
 import Typography from "@mui/material/Typography";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import {Tooltip} from "@mui/material";
+import CreateNewFolderOutlinedIcon from "@mui/icons-material/CreateNewFolderOutlined";
+import NoteAddOutlinedIcon from "@mui/icons-material/NoteAddOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
 const sampleData = [
-  "test.js",
-  "launch.py",
-  "config.json",
-  ["directory", "python.py", "test.py", ["dir2", "config.yaml"]],
-  ["dir3", "world.launch"],
+  {
+    type: "folder",
+    name: "catkin_ws",
+    children: [
+      {
+        type: "folder",
+        name: "src",
+        children: [
+          {type: "file", name: "test.py"},
+          {
+            type: "folder",
+            name: "clover",
+            children: [{type: "file", name: "clover.world"}],
+          },
+        ],
+      },
+      {type: "file", name: "flight.py"},
+    ],
+  },
+  {type: "file", name: "package.json"},
 ];
 
 export default function FileManager() {
   const Directory = props => {
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = React.useState(false);
+    const [showActions, setShowActions] = React.useState("none");
+    return (
+      <>
+        <Box
+          onClick={() => {
+            setOpen(prev => !prev);
+          }}
+          height={"30px"}
+          width={"100%"}
+          display={"flex"}
+          justifyContent={"space-between"}
+          onMouseOver={() => {
+            setShowActions("flex");
+          }}
+          onMouseOut={() => {
+            setShowActions("none");
+          }}
+          sx={{
+            "&:hover": {bgcolor: "background.cloverAppBar"},
+            pl: `${props.level * 10}px`,
+            pr: "7px",
+            cursor: "pointer",
+          }}>
+          <Box
+            display={"flex"}
+            gap={"7px"}
+            alignItems={"center"}
+            height={"30px"}>
+            <ArrowForwardIosRoundedIcon
+              sx={{
+                color: "primary.50",
+                width: "18px",
+                transform: `rotate(${open ? "90deg" : "0deg"})`,
+                transition: "transform 0.1s",
+              }}
+            />
+            <img
+              src={getMaterialFolderIcon(props.name)}
+              alt={props.name}
+              width="18px"
+              height={"18px"}
+            />
+            <Typography
+              color={"#b0b1b2"}
+              sx={{
+                fontSize: "14px",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                maxWidth: "70%",
+              }}>
+              {props.name}
+            </Typography>
+          </Box>
+          <Box
+            gap={"4px"}
+            alignItems={"center"}
+            height={"30px"}
+            display={showActions}
+            mr={`${props.level * 10}px`}>
+            <NoteAddOutlinedIcon
+              sx={{
+                fontSize: "18px",
+                color: "rgba(176,177,178,0.5)",
+                "&:hover": {color: "#b0b1b2"},
+              }}
+            />
+            <CreateNewFolderOutlinedIcon
+              sx={{
+                fontSize: "18px",
+                color: "rgba(176,177,178,0.5)",
+                "&:hover": {color: "#b0b1b2"},
+              }}
+            />
+            <EditOutlinedIcon
+              sx={{
+                fontSize: "18px",
+                color: "rgba(176,177,178,0.5)",
+                "&:hover": {color: "#b0b1b2"},
+              }}
+            />
+            <DeleteOutlinedIcon
+              sx={{
+                fontSize: "18px",
+                color: "rgba(176,177,178,0.5)",
+                "&:hover": {color: "#b0b1b2"},
+              }}
+            />
+          </Box>
+        </Box>
+        <Box display={open ? "visible" : "none"}>{props.children}</Box>
+      </>
+    );
+  };
+
+  const File = props => {
     return (
       <Box
-        onClick={() => {
-          setOpen(prev => !prev);
-        }}
         display={"flex"}
         gap={"7px"}
-        height={"40px"}
+        height={"30px"}
         width={"100%"}
         alignItems={"center"}
         sx={{
           "&:hover": {bgcolor: "background.cloverAppBar"},
-          pl: "7px",
+          pl: `${props.level * 10 + 25}px`,
           pr: "7px",
           cursor: "pointer",
         }}>
-        <ArrowForwardIosRoundedIcon
-          sx={{
-            color: "primary.50",
-            width: "18px",
-            transform: `rotate(${open ? "90deg" : "0deg"})`,
-            transition: "transform 0.1s",
-          }}
-        />
         <img
-          src={getMaterialFolderIcon("dir")}
-          alt="directory"
+          src={getMaterialFileIcon(props.name)}
+          alt={props.name}
           width="18px"
           height={"18px"}
         />
@@ -64,6 +167,23 @@ export default function FileManager() {
         </Typography>
       </Box>
     );
+  };
+
+  let level = 0;
+  const FileTree = ({data}) => {
+    level++;
+    return data.map(item => {
+      if (item.type === "file") {
+        return <File name={item.name} level={level} key={item.name} />;
+      }
+      if (item.type === "folder") {
+        return (
+          <Directory name={item.name} level={level} key={item.name}>
+            <FileTree data={item.children} />
+          </Directory>
+        );
+      }
+    });
   };
 
   return (
@@ -90,7 +210,7 @@ export default function FileManager() {
             />
           </Tooltip>
         </Box>
-        <Directory name={"FILES"} />
+        <FileTree data={sampleData} />
       </Box>
     </ThemeProvider>
   );
